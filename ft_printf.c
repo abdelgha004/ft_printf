7bdd043b602995cd678c582c7ea9e726fb4bf970
aakourya <abdelghafourakouryah@gmail.com>
2025-11-10 17:19:18 +0100

    first step

--- ft_printf.c
+++ ft_printf.c
@@ -6,11 +6,74 @@
 /*   By: aakourya <aakourya@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/08 09:58:37 by aakourya          #+#    #+#             */
-/*   Updated: 2025/11/08 21:40:44 by aakourya         ###   ########.fr       */
+/*   Updated: 2025/11/10 17:16:47 by aakourya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libftprintf.h"
 
-int ft_printf(const char *format, ...){
-    
+static int	ft_putarg(char C, va_list *args)
+{
+	char	ch;
+
+	if (C == 'c')
+	{
+		ch = va_arg(*args, int);
+		return (write(1, &ch, 1));
+	}
+	else if (C == 's')
+	{
+	}
+	else if (C == 'p')
+	{
+	}
+	else if (C == 'd')
+	{
+	}
+	else if (C == 'i')
+	{
+	}
+	else if (C == 'u')
+	{
+	}
+	else if (C == 'x')
+	{
+	}
+	else if (C == 'X')
+	{
+	}
+	else if (C == '%')
+	{
+		return (write(1, "%", 1));
+	}
+	return (0);
+}
+
+int	ft_printf(char *format, ...)
+{
+	va_list	args;
+	int		count;
+	int		i;
+
+	if (!format)
+		return (-1);
+	count = 0;
+	i = 0;
+	va_start(args, format);
+	while (format[i])
+	{
+		if (format[i] == '%' && format[i + 1])
+		{
+			count += write(1, format, i);
+			i++;
+			count += ft_putarg(format[i], &args);
+			format += (i + 1);
+			i = 0;
+		}
+		else
+			i++;
+	}
+	count += write(1, format, i);
+	va_end(args);
+	return (count);
 }
--- libftprintf.h
+++ libftprintf.h
@@ -1,19 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aakourya <aakourya@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/08 21:52:58 by aakourya          #+#    #+#             */
-/*   Updated: 2025/11/08 21:52:59 by aakourya         ###   ########.fr       */
+/*   Updated: 2025/11/10 17:14:43 by aakourya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-int ft_printf(const char *, ...);
+# include <stdarg.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 
+int	ft_printf(char *format, ...);
 
 #endif
